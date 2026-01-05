94c9464aa26ca2dcf612927f47fe530ddc8b6569
JuanPablo0612 <gallegojuanpablo39@gmail.com>
2026-01-05 12:58:38 -0500

    add LoginScreen composable and preview

---
+++ composeApp/src/commonMain/kotlin/com/juanpablo0612/tucargo/features/auth/presentation/login/LoginScreen.kt
@@ -0,0 +1,18 @@
+package com.juanpablo0612.tucargo.features.auth.presentation.login
+
+import androidx.compose.runtime.Composable
+import com.juanpablo0612.tucargo.core.ui.theme.TuCargoTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreenContent() {
+
+}
+
+@Preview
+@Composable
+fun LoginScreenContentPreview() {
+    TuCargoTheme {
+        LoginScreenContent()
+    }
+}
